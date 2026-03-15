a1a7cabf1c835e95f8156d02e4ea769eb51d67a8
Omkar Kolangade <omkar179@gmail.com>
2026-03-15 07:04:52 -0700

    Initial Commit

---
+++ InfiLooper/InfiLooperApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiLooperApp.swift
+//  InfiLooper
+//
+//  Created by Omkar Kolangade on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct InfiLooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
